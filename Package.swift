eee4ce847c9f413313796a43eef154a137f05907
MahdiBM <54685446+MahdiBM@users.noreply.github.com>
2022-01-11 17:16:20 +0330

    added rate-testing stuff

--- Package.swift
+++ Package.swift
@@ -1,23 +1,25 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "hello",
-    platforms: [.macOS(.v10_14)],
+    name: "CRRateLimitTester",
+    platforms: [.macOS(.v12)],
     products: [
         .executable(name: "Server", targets: ["Server"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.11.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.6.0")
     ],
     targets: [
         .target(name: "App",
             dependencies: [
                 .product(name: "Hummingbird", package: "hummingbird"),
                 .product(name: "HummingbirdFoundation", package: "hummingbird"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
@@ -26,16 +28,10 @@ let package = Package(
                 .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
-        .target(name: "Server",
+        .executableTarget(name: "Server",
             dependencies: [
                 .byName(name: "App"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ]
-        ),
-        .testTarget(name: "AppTests",
-            dependencies: [
-                .byName(name: "App"),
-                .product(name: "HummingbirdXCT", package: "hummingbird")
             ]
         )
     ]
--- Sources/App/Application+configure.swift
+++ Sources/App/Application+configure.swift
@@ -1,5 +1,7 @@
 import Hummingbird
 import HummingbirdFoundation
+import AsyncHTTPClient
+import struct Foundation.Date
 
 extension HBApplication {
     /// configure your application
@@ -7,9 +9,58 @@ extension HBApplication {
     /// setup the encoder/decoder
     /// add your routes
     public func configure() throws {
-        middleware.add(HBFileMiddleware(application: self))
-        router.get("/") { _ in
-            return "Hello"
+        
+        self.httpClient = HTTPClient(eventLoopGroupProvider: .shared(self.eventLoopGroup))
+        
+        let token = HBEnvironment.shared.get("token")!
+        
+        router.get("test-rate") { req -> EventLoopFuture<String> in
+            let rate: Int
+            let url: String
+            let request: HTTPClient.Request
+            do {
+                rate = try req.uri.queryParameters.require("rate", as: Int.self)
+                url = try req.uri.queryParameters.require("url")
+                request = try HTTPClient.Request(
+                    url: url,
+                    method: .GET,
+                    headers: ["Authorization": "Bearer \(token)"]
+                )
+            } catch {
+                return req.context.eventLoop.makeFailedFuture(error)
+            }
+            
+            let start = Date()
+            
+            return (0..<rate).map { idx in
+                self.httpClient.execute(request: request).flatMapThrowing { res -> Void in
+                    if res.status == .tooManyRequests {
+                        throw HBHTTPError(.tooManyRequests)
+                    } else if res.status != .ok {
+                        throw HBHTTPError(res.status)
+                    }
+                    return
+                }
+            }.reduce(into: self.eventLoopGroup.next().makeSucceededVoidFuture()) { result, next in
+                result = result.flatMap({ next })
+            }.map { _ -> String in
+                let end = Date()
+                let interval = end.timeIntervalSinceReferenceDate
+                - start.timeIntervalSinceReferenceDate
+                return "Took \(interval) seconds for \(rate) requests."
+            }
+        }
+    }
+}
+
+// MARK: - httpClient
+extension HBApplication {
+    var httpClient: HTTPClient {
+        get { self.extensions.get(\.httpClient) }
+        set {
+            self.extensions.set(\.httpClient, value: newValue, shutdownCallback: { httpClient in
+                try httpClient.syncShutdown()
+            })
         }
     }
 }
